7dbfc5356d6b2274df7bfbfbaadd59743d3b1c24
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2024-03-03 22:04:22 -0800

    Create playPreviousSong function

--- index.js
+++ index.js
@@ -121,3 +121,13 @@ const playNextSong = () => {
     playSong(nextSong.id);
   }
 };
+
+const playPreviousSong = () => {
+  if (userData?.currentSong === null) return;
+  else {
+    const currentSongIndex = getCurrentSongIndex();
+    const previousSong = userData?.songs[currentSongIndex - 1];
+
+    playSong(previousSong.id);
+  }
+};
